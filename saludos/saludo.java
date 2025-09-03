56606f8d874244a1b448c828296e05fb08f9b45b
aaronvasq <160943492+aaronvasq@users.noreply.github.com>
2025-09-03 13:10:41 -0400

    Update saludo.java

--- saludos/saludo.java
+++ saludos/saludo.java
@@ -1,9 +1,9 @@
 public class main{
     public static void main(String[] args){
         System.out.println("Hola equipo");
-        System.out.println("1 MAURICIO MARCO ZZAMORA RIVAS");
-        System.out.println("BEYMAR ERWIN UGARTE AGUILAR");
-        System.out.println("ELMER JHON LIMA MAMANI");
-        System.out.println("AARON EDDY VASQUEZ ALI");
+        System.out.println("LIMA MAMANI ELMER JHON");
+        System.out.println("UGARTE AGUILAR BEYMAR ERWIN");
+        System.out.println("VASQUEZ ALI AARON EDDY");
+        System.out.println("ZAMORA RIVAS MAURICIO MARCO");
     }
 }
