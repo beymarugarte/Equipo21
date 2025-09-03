666f01964e2d5937fc4f6987fe8751c1ba7b9dfc
aaronvasq <160943492+aaronvasq@users.noreply.github.com>
2025-09-03 13:12:25 -0400

    Update operacion.py

--- operacion/operacion.py
+++ operacion/operacion.py
@@ -3,7 +3,7 @@ a = 6
 b = 2
 s = a+b
 print("la suma de: ",a, "y",b,"es ",s)
-print("1 MAURICIO MARCO ZZAMORA RIVAS")
-print("2 BEYMAR ERWIN UGARTE AGUILAR")
-print("3 ELMER JHON LIMA MAMANI")
-print("4 AARON EDDY VASQUEZ ALI")
+print("1 LIMA MAMANI ELMER JHON")
+print("2 UGARTE AGUILAR BEYMAR ERWIN")
+print("3 VASQUEZ ALI AARON EDDY")
+print("4 MAURICIO MARCO ZAMORA RIVAS")
