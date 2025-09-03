32cde40f4f349c4ea8f9f920a69ec7311833b103
Beymar <beymarerwin@gmail.com>
2025-09-03 11:44:03 -0400

    mi archivo html equipo 21

---
+++ operacion/operacion.py
@@ -0,0 +1,9 @@
+#operacion simple
+a = 6
+b = 2
+s = a+b
+print("la suma de: ",a, "y",b,"es ",s)
+print("1 MAURICIO MARCO ZZAMORA RIVAS")
+print("2 BEYMAR ERWIN UGARTE AGUILAR")
+print("3 ELMER JHON LIMA MAMANI")
+print("4 AARON EDDY VASQUEZ ALI")
---
+++ saludos/saludo.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        System.out.println("Hola equipo");
+        System.out.println("1 MAURICIO MARCO ZZAMORA RIVAS");
+        System.out.println("BEYMAR ERWIN UGARTE AGUILAR");
+        System.out.println("ELMER JHON LIMA MAMANI");
+        System.out.println("AARON EDDY VASQUEZ ALI");
+    }
+}
